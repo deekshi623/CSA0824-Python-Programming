047b61d5f04b85379368ad20325f7a0865232b22
deekshi623 <122506748+deekshi623@users.noreply.github.com>
2024-03-19 10:36:35 +0530

    Create area of circle,rectangle.py

---
+++ area of circle,rectangle.py
@@ -0,0 +1,8 @@
+import math as m
+r=float(input("enter radius:"))
+areacircle=m.pi*r*r
+print(f"area of circle is{areacircle}")
+l=float(input("enter length:"))
+b=float(input("enter base:"))
+arearect=l*b
+print(f"area of rectangle is{arearect}")
