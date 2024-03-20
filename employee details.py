8c7ea669810cc409ad72b07b63b21ce7d1195fdb
deekshi623 <122506748+deekshi623@users.noreply.github.com>
2024-03-20 10:30:58 +0530

    Add files via upload

---
+++ employee details.py
@@ -0,0 +1,11 @@
+emp_name=input("enter a employee name")
+emp_id=int(input("enter a employee id"))
+emp_phoneno=int(input("enter a employee phone no"))
+emp_adress=input("enter a employee adress")
+emp_salaray=int(input("enter a employee salary"))
+print(emp_name)
+print(emp_id)
+print(emp_phoneno)
+print(emp_adress)
+print(emp_salary)
+                       
